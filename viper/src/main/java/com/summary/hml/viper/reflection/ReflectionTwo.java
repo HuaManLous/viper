6d3f8e564d41f079830f90806f31f400e5f74bda
花满楼 <1729375118@qq.com>
2021-11-11 18:02:18 +0800

    fix ==> 反射 study

---
+++ viper/src/main/java/com/summary/hml/viper/reflection/ReflectionTwo.java
@@ -0,0 +1,118 @@
+package com.summary.hml.viper.reflection;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * ♪───Ｏ（≧∇≦）Ｏ────♪
+ *
+ * @Author ： 花满楼
+ * @CreateTime ： 2021-11-11 17:02
+ * @Description : 反射
+ */
+public class ReflectionTwo {
+
+    public static void main(String[] args) throws Exception{
+        Class<?> c1 = Class.forName("com.summary.hml.viper.reflection.Student");
+        // 1。获取包名和类名
+        String name = c1.getName();
+        System.out.println("获取类的名字(包名和类名)："+ name);
+        // 2。获取类名
+        String simpleName = c1.getSimpleName();
+        System.out.println("获取类名:"+simpleName);
+        // 3.获得类的属性只能是public
+        Field[] fields = c1.getFields();
+        for (Field field : fields) {
+            System.out.println("属性:"+field);
+        }
+
+        // 3.获取全部属性
+        Field[] declaredField = c1.getDeclaredFields();
+        for (Field f : declaredField ) {
+            System.out.println("全部属性:"+f);
+        }
+
+        // 4.获取指定的属性
+        Field name1 = c1.getField("name");
+        System.out.println("指定的属性："+name1);
+
+        // 5.获取方法-并执行方法
+        Constructor<?> constructor = c1.getConstructor();
+        Student student = (Student)constructor.newInstance();
+        Method study = c1.getMethod("study",String.class);
+        study.invoke(student,"花满楼");
+
+        // 6.给属性赋值
+        Field age = c1.getDeclaredField("age");
+        // 私有要关闭
+        age.setAccessible(true);
+        age.set(student,"1");
+        System.out.println(student);
+
+
+        // 5.获取反射
+        /*AnnotationWithReflection annotation = c1.getAnnotation(AnnotationWithReflection.class);
+        String name2 = annotation.name();
+        System.out.println("反射获取注解的值：" + name2);*/
+
+    }
+
+}
+
+class Student{
+    public String name;
+
+    private String age;
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @AnnotationWithReflection(name = "花满楼-自定义")
+    public void study(String name){
+        System.out.println(name + ":学习");
+    }
+
+    public Student() {
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public Student(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
+
+@Target(value = ElementType.METHOD)
+@Retention(value = RetentionPolicy.RUNTIME)
+@interface AnnotationWithReflection{
+    String name() default "花满楼";
+}
