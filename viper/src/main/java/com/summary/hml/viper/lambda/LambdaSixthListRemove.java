cd52b9bb0c4cb53e79e52113468d9cc5039e40f9
花满楼-埃森哲 <1729375118@qq.com>
2021-11-15 16:14:24 +0800

    fix ==> lambda实现list集合移除指定的元素

---
+++ viper/src/main/java/com/summary/hml/viper/lambda/LambdaSixthListRemove.java
@@ -0,0 +1,43 @@
+package com.summary.hml.viper.lambda;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * ♪───Ｏ（≧∇≦）Ｏ────♪
+ *
+ * @Author ： 花满楼
+ * @CreateTime ： 2021-10-29 15:17
+ * @Description : list集合移除指定的元素
+ */
+public class LambdaSixthListRemove {
+
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("1111");
+        list.add("2222");
+        list.add("3333");
+        list.add("2222");
+        list.add("1111");
+        list.add("5555");
+
+        // 通过迭代的方式移除指定元素
+        Iterator<String> iterator = list.iterator();
+        while(iterator.hasNext()) {
+            if ("1111".equals(iterator.next())) {
+                iterator.remove();  
+            }
+        }
+
+        // 通过lambda方式移除指定的元素
+        list.removeIf( e -> {
+           if (e.equals("2222")) {
+               return true;
+           }
+           return false;
+        });
+
+        System.out.println("list = " + list);
+    }
+    
+}
