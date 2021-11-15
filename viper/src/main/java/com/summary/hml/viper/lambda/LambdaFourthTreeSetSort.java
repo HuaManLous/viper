582d74997f4a63addc6d209fb9b3041b0787c68a
花满楼-埃森哲 <1729375118@qq.com>
2021-11-15 15:10:49 +0800

    fix ==> lambda实现treeSet集合的排序

--- viper/src/main/java/com/summary/hml/viper/lambda/LambdaFourthTreeSetSort.java
+++ viper/src/main/java/com/summary/hml/viper/lambda/LambdaFourthTreeSetSort.java
@@ -13,7 +13,9 @@ import java.util.TreeSet;
 public class LambdaFourthTreeSetSort {
 
     public static void main(String[] args) {
-        Set<Person2> set = new TreeSet<>();
+        Set<Person2> set = new TreeSet<Person2>((o1, o2) -> {
+            return o1.getAge() - o2.getAge();
+        });
         set.add(new Person2("画满楼_01",10));
         set.add(new Person2("画满楼_02",11));
         set.add(new Person2("画满楼_03",12));
@@ -22,6 +24,39 @@ public class LambdaFourthTreeSetSort {
         set.add(new Person2("画满楼_06",7));
         set.add(new Person2("画满楼_07",50));
         set.add(new Person2("画满楼_08",50));
+        System.out.println("set = " + set);
+
+        // 上面的代码打印的只能是不重复的
+        Set<Person2> set2 = new TreeSet<Person2>((o1, o2) -> {
+            if (o1.getAge() >= o2.getAge()) {
+                return 1;
+            }
+            return -1;
+        });
+        set2.add(new Person2("画满楼_01",10));
+        set2.add(new Person2("画满楼_02",11));
+        set2.add(new Person2("画满楼_03",12));
+        set2.add(new Person2("画满楼_04",9));
+        set2.add(new Person2("画满楼_05",8));
+        set2.add(new Person2("画满楼_06",7));
+        set2.add(new Person2("画满楼_07",50));
+        set2.add(new Person2("画满楼_08",50));
+        System.out.println("set2 = " + set2);
+
+
+        Set<Person2> set3 = new TreeSet<Person2>((o1, o2) -> {
+            // 通过三目运算符
+            return o1.getAge()-o2.getAge() >= 0 ? 1:-1;
+        });
+        set3.add(new Person2("画满楼_01",10));
+        set3.add(new Person2("画满楼_02",11));
+        set3.add(new Person2("画满楼_03",12));
+        set3.add(new Person2("画满楼_04",9));
+        set3.add(new Person2("画满楼_05",8));
+        set3.add(new Person2("画满楼_06",7));
+        set3.add(new Person2("画满楼_07",50));
+        set3.add(new Person2("画满楼_08",50));
+        System.out.println("set3 = " + set3);
 
     }
 
