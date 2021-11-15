226fc3b8276b59499ebc591f308c9717bb7575e9
花满楼-埃森哲 <1729375118@qq.com>
2021-11-15 15:59:18 +0800

    fix ==> lambda实现list集合的遍历

---
+++ viper/src/main/java/com/summary/hml/viper/lambda/LambdaFifthListForeach.java
@@ -0,0 +1,34 @@
+package com.summary.hml.viper.lambda;
+
+import java.util.ArrayList;
+
+/**
+ * ♪───Ｏ（≧∇≦）Ｏ────♪
+ *
+ * @Author ： 花满楼
+ * @CreateTime ： 2021-10-29 15:17
+ * @Description : 集合的遍历
+ */public class LambdaFifthListForeach {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("1111");
+        list.add("2222");
+        list.add("3333");
+        list.add("2222");
+        list.add("1111");
+        list.add("5555");
+        list.forEach((a)->{
+            System.out.println("a = " + a);
+        });
+        // 方法的引用
+        list.forEach(System.out::println);
+
+
+
+
+    }
+
+
+}
