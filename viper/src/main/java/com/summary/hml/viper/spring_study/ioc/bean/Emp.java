5809d09bd7fb17fc16074004f24da9c2f02564e5
花满楼 <1729375118@qq.com>
2023-03-21 23:10:25 +0800

    fix ==> JSON.toJSONString(user, SerializerFeature.WriteNullStringAsEmpty)  打印null

--- viper/src/main/java/com/summary/hml/viper/spring_study/ioc/bean/Emp.java
+++ viper/src/main/java/com/summary/hml/viper/spring_study/ioc/bean/Emp.java
@@ -1,11 +1,14 @@
 package com.summary.hml.viper.spring_study.ioc.bean;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * ♪───Ｏ（≧∇≦）Ｏ────♪
@@ -64,5 +67,14 @@ public class Emp {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("iocxml/InnerClassBean.xml");
         Emp emp = applicationContext.getBean("emp", Emp.class);
         System.out.println("emp = " + JSON.toJSONString(emp));
+
+        Map<String,Object> hashMap = new HashMap<>();
+        hashMap.put("name",null);
+        hashMap.put("age",18);
+        // JSON.toJSONString 会去掉null值
+        System.out.println("JSON.toJSONString(hashMap) = " + JSON.toJSONString(hashMap));
+
+        System.out.println("JSON.toJSONString(hashMap, null) = " + JSON.toJSONString(hashMap, SerializerFeature.WriteNullStringAsEmpty));
+
     }
 }
