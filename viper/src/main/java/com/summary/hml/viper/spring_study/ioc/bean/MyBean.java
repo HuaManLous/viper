63a33f0afab0a6ba6703d9433cafa229dc1a440a
花满楼 <1729375118@qq.com>
2023-03-22 15:02:28 +0800

    docx ==> spring-ioc DI bean管理 工厂bean  实现FactoryBean  重写getObject() 方法

---
+++ viper/src/main/java/com/summary/hml/viper/spring_study/ioc/bean/MyBean.java
@@ -0,0 +1,43 @@
+package com.summary.hml.viper.spring_study.ioc.bean;
+
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * ♪───Ｏ（≧∇≦）Ｏ────♪
+ *
+ * @Author ： 花满楼
+ * @CreateTime ： 2023-03-22 14:52
+ * @Description : TODO
+ */
+public class MyBean implements FactoryBean<Dept> {
+
+
+    @Override
+    public Dept getObject() throws Exception {
+        Dept dept = new Dept();
+        dept.setName("factoryBean -- name ");
+        dept.setAddress("factoryBean -- address ");
+        return dept;
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return null;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return false;
+    }
+    
+    @Test
+    public void test() {
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext("iocxml/FactoryBean.xml");
+        Dept myBeanDept = beanFactory.getBean("myBean", Dept.class);
+        System.out.println("myBeanDept = " + myBeanDept);
+
+    }
+}
