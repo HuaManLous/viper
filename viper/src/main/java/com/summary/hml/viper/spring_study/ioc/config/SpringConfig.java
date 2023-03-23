2f8c39c7c0efb7b00eca917b9078d2a3ff1db85e
花满楼 <1729375118@qq.com>
2023-03-23 14:30:47 +0800

    feat ==> spring ioc 全注解  去掉xml文件

---
+++ viper/src/main/java/com/summary/hml/viper/spring_study/ioc/config/SpringConfig.java
@@ -0,0 +1,18 @@
+package com.summary.hml.viper.spring_study.ioc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * ♪───Ｏ（≧∇≦）Ｏ────♪
+ *
+ * @Author ： 花满楼
+ * @CreateTime ： 2023-03-23 14:24
+ * @Description : TODO
+ */
+@Configuration // 配置类
+@ComponentScan("com.summary.hml.viper.spring_study")
+public class SpringConfig {
+
+
+}
