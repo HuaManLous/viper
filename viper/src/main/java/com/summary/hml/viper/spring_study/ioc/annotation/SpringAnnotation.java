d9937adcfe02bfc678e0c738fb8194770fb90cd7
花满楼 <1729375118@qq.com>
2023-03-23 14:13:17 +0800

    feat ==> spring ioc 自动装配 @Autowired @Qualifier @Resource @Value

--- viper/src/main/java/com/summary/hml/viper/spring_study/ioc/annotation/SpringAnnotation.java
+++ viper/src/main/java/com/summary/hml/viper/spring_study/ioc/annotation/SpringAnnotation.java
@@ -1,10 +1,12 @@
 package com.summary.hml.viper.spring_study.ioc.annotation;
 
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * ♪───Ｏ（≧∇≦）Ｏ────♪
@@ -25,9 +27,31 @@ public class SpringAnnotation {
      *
      */
 
+    /**
+     *
+     * @Autowired  按照类型自动注入
+     * @Qualifier  按照名称注入（必须和@Autowired一起使用）
+     * @Resource   可以按照名称和类型注入  先按照名称注入，然后根据类型，如果都不行就报错
+     * @Value      给普通属性注入
+     *     1.不能作用于静态变量；
+     *     2.不能作用于常量;
+     *     3.不能在非注册的类中使用；
+     *     4.使用这个类时，只能通过依赖注入的方式，用new的方式是不会自动注入这些配置的。
+     *
+     */
+
+    //@Value(value = "张三")
+    @Value("张三")
+    private String name;
+
+    //@Autowired  // 按照属性类型注入
+    //@Qualifier(value = "springAnnotationDaoImpl")
+    @Resource(name = "springAnnotationDaoImpl")
+    private SpringAnnotationDao springAnnotationDao;
 
     public void add() {
-        System.out.println("service ....");
+        System.out.println("service ...." + name);
+        springAnnotationDao.add();
     }
 
     @Test
@@ -35,7 +59,6 @@ public class SpringAnnotation {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("iocxml/SpringAnnotation.xml");
         SpringAnnotation springAnnotation = applicationContext.getBean("springAnnotation", SpringAnnotation.class);
         springAnnotation.add();
-
     }
 
 
