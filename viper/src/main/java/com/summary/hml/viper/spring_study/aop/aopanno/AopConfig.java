804d0b5a42fc277a9fae30bc8f1db5544427c071
花满楼 <1729375118@qq.com>
2023-03-25 00:41:39 +0800

    feat ==> spring aop全注解开发配置类

---
+++ viper/src/main/java/com/summary/hml/viper/spring_study/aop/aopanno/AopConfig.java
@@ -0,0 +1,27 @@
+package com.summary.hml.viper.spring_study.aop.aopanno;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * ♪───Ｏ（≧∇≦）Ｏ────♪
+ *
+ * @Author ： 花满楼
+ * @CreateTime ： 2023-03-25 00:36
+ * @Description : aspect配置类
+ */
+@Configuration
+@ComponentScan("com.summary.hml.viper.spring_study.aop.aopanno")
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class AopConfig {
+
+    // 完全注解开发  可以省略下面的配置文件步骤
+    /**
+     *  <context:component-scan base-package="com.summary.hml.viper.spring_study"/>
+     *  <aop:aspectj-autoproxy></aop:aspectj-autoproxy>
+     *
+     */
+
+
+}
