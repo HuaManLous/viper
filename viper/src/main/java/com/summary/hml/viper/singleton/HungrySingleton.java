c709eef9d3a18125b1655fcae8128b3189a5f3ce
花满楼 <1729375118@qq.com>
2023-03-28 13:52:00 +0800

    feat ==> 饿汉式序列化时添加readResolve

--- viper/src/main/java/com/summary/hml/viper/singleton/HungrySingleton.java
+++ viper/src/main/java/com/summary/hml/viper/singleton/HungrySingleton.java
@@ -1,5 +1,8 @@
 package com.summary.hml.viper.singleton;
 
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+
 /**
  * ♪───Ｏ（≧∇≦）Ｏ────♪
  *
@@ -7,7 +10,7 @@ package com.summary.hml.viper.singleton;
  * @CreateTime ： 2023-03-28 11:24
  * @Description : 饿汉式
  */
-public class HungrySingleton {
+public class HungrySingleton implements Serializable {
 
     /**
      *
@@ -24,6 +27,15 @@ public class HungrySingleton {
 
     // 提供公有的静态的获取对象实例的方法
     public static HungrySingleton getInstance() {
+        return hungrySingleton;
+    }
+
+
+    /**
+     * 虽然增加readResolve()方法返回实例解决了单例模式被破坏的问题，
+     *     但是实际上实例化了两次，只不过新创建的对象没有被返回而已。
+     */
+    private Object readResolve() throws ObjectStreamException {
         return hungrySingleton;
     }
 
