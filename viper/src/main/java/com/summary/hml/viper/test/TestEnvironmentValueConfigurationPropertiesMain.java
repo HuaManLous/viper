3fd87b825b0a750e8e5c7806bf3cc993028d014f
花满楼 <1729375118@qq.com>
2023-05-31 17:07:52 +0800

    feat ==> springboot获取配置文件的三种方式@Value

--- viper/src/main/java/com/summary/hml/viper/test/TestEnvironmentValueConfigurationPropertiesMain.java
+++ viper/src/main/java/com/summary/hml/viper/test/TestEnvironmentValueConfigurationPropertiesMain.java
@@ -12,7 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description : 测试的类
  */
 @RestController
-public class TestDemo {
+public class TestEnvironmentValueConfigurationPropertiesMain {
+
+    /**
+     *  a.使用 @Value 读取配置文件
+     *  b.使用 @ConfigurationProperties 读取配置文件
+     *  c.使用 Environment 读取配置文件
+     */
 
     @Value("${spring.environment.name}")
     private String a;
