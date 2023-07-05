5a2920fe933932f71be06747c7fecdd88b776b62
花满楼 <1729375118@qq.com>
2023-07-05 11:22:19 +0800

    feat ==> 请求路径修改

--- viper/src/main/java/com/summary/hml/viper/ssm/controller/TestController.java
+++ viper/src/main/java/com/summary/hml/viper/ssm/controller/TestController.java
@@ -2,7 +2,9 @@ package com.summary.hml.viper.ssm.controller;
 
 import com.summary.hml.viper.ssm.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -24,5 +26,16 @@ public class TestController {
     public String get() {
         return testService.getById("11");
     }
+
+    @RequestMapping("use2/{id}")
+    public String get2(@PathVariable("id") String id) {
+        return testService.getById(id);
+    }
+
+    @RequestMapping("use3/")
+    public String get3(@RequestParam String id) {
+        return testService.getById(id);
+    }
+
 
 }
