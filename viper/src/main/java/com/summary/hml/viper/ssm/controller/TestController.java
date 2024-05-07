da46ff10781a0cc751f9d08489dc3edd20b4f214
花满楼 <1729375118@qq.com>
2024-05-07 15:49:08 +0800

    Fest ==> 两个list的差

--- viper/src/main/java/com/summary/hml/viper/ssm/controller/TestController.java
+++ viper/src/main/java/com/summary/hml/viper/ssm/controller/TestController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * ♪───Ｏ（≧∇≦）Ｏ────♪
  *
@@ -37,5 +40,32 @@ public class TestController {
         return testService.getById(id);
     }
 
+
+    public static void main(String[] args) {
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(2);
+        list2.add(3);
+        list2.add(4);
+
+        // 找到交集
+        List<Integer> intersection = new ArrayList<>(list1);
+        intersection.retainAll(list2);
+        System.out.println("交集：" + intersection);
+
+        // 找到list1中与list2不同的部分
+        List<Integer> difference1 = new ArrayList<>(list1);
+        difference1.removeAll(intersection);
+        System.out.println("list1中与list2不同的部分：" + difference1);
+
+        // 找到list2中与list1不同的部分
+        List<Integer> difference2 = new ArrayList<>(list2);
+        difference2.removeAll(intersection);
+        System.out.println("list2中与list1不同的部分：" + difference2);
+    }
 
 }
